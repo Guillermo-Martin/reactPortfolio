160371d00105307bd1a49a0790373b85a9f9e3ed
gcmartin <gscalica@gmail.com>
2020-05-23 21:29:37 -0700

    Create assets folder and edit About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './../assets/styles/about.css'
 
 
 function About() {
   return (
-    <h1>THIS IS THE ABOUT PAGE</h1>
-  );  
+    <div>
+      <div className="container">
+        
+        <h1>About Me</h1>
+          <img src="https://ca.slack-edge.com/TUFNTT7ND-UURHQ9VPE-194f27264248-512" alt="Guillermo" />
+          
+          <div className="row">
+            <p>
+              Hello! My name's Guillermo. I'm from Virginia Beach, Virginia. I have a Bachelor's of Science Degree in Biology from James Madison University and a Master's of Science Degree in Forensic Science with a specialization in criminalistics from the University of Central Oklahoma. I moved to the Bay Area more than 10 years ago when I got a job at the Alameda County Sheriff's Office Crime Lab as a forensic scientist. I worked there for more than 10 years, primarily in the Forensic Biology and DNA unit. I also got to work in latent prints and processed crime scenes.
+            </p>
+
+            <p>
+              During the last few years I was there, I gained an interest in coding, particularly in web development. I learned about web development through a class offered by Udemy and really enjoyed it. I decided to quit my job as a forensic scientist to pursue a career in web development. I'm currently enrolled in a coding bootcamp offered by UC Berkeley Extension and I hope to become a front-end web developer.
+            </p>
+
+            <p>
+              In my free time, I enjoy eating, exploring the Bay Area, playing video games, reading, and fencing.
+            </p>
+          </div>
+          
+
+        
+      </div>
+    </div>
+  );
 }
 
 export default About;
