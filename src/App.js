ca8aa714438e399a92074eb8c5c22a6eb57eac6b
gcmartin <gscalica@gmail.com>
2020-05-23 20:39:45 -0700

    Create About and Contact pages and connect them to navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Route exact path='/' component={About} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
         <Route exact path='/portfolio' component={Portfolio} />
       </div>
     </Router>
