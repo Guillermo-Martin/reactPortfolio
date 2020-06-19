38a5f1a18493a6554b657114ca9ca0378ab98b36
gcmartin <gscalica@gmail.com>
2020-06-19 10:28:49 -0700

    Add skills to About page and change font size

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,20 +32,32 @@ function About() {
 
           {/* Column for About Me */}
           <div className="col">
-            <p>
+            <p className="aboutText">
               I'm from Virginia Beach, Virginia. I have a Bachelor's of Science Degree in Biology from James Madison University and a Master's of Science Degree in Forensic Science with a specialization in criminalistics from the University of Central Oklahoma. I moved to the Bay Area more than 10 years ago when I got a job at the Alameda County Sheriff's Office Crime Lab as a forensic scientist. I worked there for more than 10 years, primarily in the Forensic Biology and DNA unit. I also got to work in latent prints and processed crime scenes.  During the last few years I was there, I gained an interest in coding, particularly in web development. I learned about web development through a class offered by Udemy and really enjoyed it. I decided to quit my job as a forensic scientist to pursue a career in web development.
             </p>
       
-            <p>
+            <p className="aboutText">
               I recently earned my certificate in full-stack web development from the University of California, Berkeley Extension.  I have experience and am proficient with the MERN stack (MongoDB, Express.js, React.js, and Node.js).  During my studies, I developed a passion for front-end web development and design and have come to love using React.  Seeing beautifully designed websites that provide a great user experience motivates me to create the same and learn more about front-end technologies and design frameworks in order to provide a pleasant user experience.
             </p>
 
-            <p>
+            <p className="aboutText">
               In my spare time, I enjoy playing video games, reading, exercising, exploring all that the Bay Area has to offer, and fencing.
             </p>
 
-            <p>
+            <p className="aboutText">
               Check out my <Link to="/portfolio" >portfolio</Link>!
+            </p>
+
+            <p className="aboutText">
+            I'm experienced in:
+              <ul>
+                <li>Browser-based Technologies:  HTML5, CSS, JavaScript, jQuery, Bootstrap, and React.js</li>
+                <li>Server-side Technologies:  Node.js and Express.js</li>
+                <li>Databases:  MySQL, MongoDB, and Mongoose</li>
+                <li>API Interaction:  API, JSON, and AJAX</li>
+                <li>Deployment:  Git, GitHub Pages, and Heroku</li>
+                <li>Other:  Command Line, MVC, Computer Science applied to JavaScript, Quality Assurance, Redux, User Authentication</li>
+              </ul>
             </p>
           </div>
 
